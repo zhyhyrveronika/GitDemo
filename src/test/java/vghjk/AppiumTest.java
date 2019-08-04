07c131cfc67d5d99bf811094681f8252e6cbed4c
zhyhyrveronika <gigirver@gmail.com>
2019-08-04 12:35:02 +0300

    devel

--- src/test/java/vghjk/AppiumTest.java
+++ src/test/java/vghjk/AppiumTest.java
@@ -7,11 +7,15 @@ public class AppiumTest {
 	public void NativeAPpAndroid()
 	{
 		System.out.println("NativeAPpAndroid");
+		System.out.println("NativeAPpAndroid");
+
 	}
 	
 	@Test
 	public void IOSApps()
 	{
 		System.out.println("IOSApps");	
+		System.out.println("NativeAPpAndroid");
+
 	}
 }
