a94a1483d98d36197f7bbd74844842bb6822ba2a
zhyhyrveronika <gigirver@gmail.com>
2019-08-04 12:25:06 +0300

    gjgjgj

--- src/test/java/vghjk/RESTAPITest.java
+++ src/test/java/vghjk/RESTAPITest.java
@@ -7,9 +7,7 @@ public class RESTAPITest {
 	@Test
 	public void postJira()
 	{
-		System.out.println("postJira");
-		System.out.println("postJira2");
-		System.out.println("postJira3");
+		
 	}
 	
 	
