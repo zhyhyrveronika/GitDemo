b5014f4a0595bf561ba1a620cb08ea087027b740
zhyhyrveronika <gigirver@gmail.com>
2019-08-04 12:15:28 +0300

    second

--- src/test/java/vghjk/RESTAPITest.java
+++ src/test/java/vghjk/RESTAPITest.java
@@ -1,14 +1,19 @@
 package vghjk;
 
 import org.testng.annotations.Test;
-//rahulonlinetutor@gmail.com
+
 public class RESTAPITest {
 
 	@Test
 	public void postJira()
 	{
 		System.out.println("postJira");
+		System.out.println("postJira2");
+		System.out.println("postJira3");
 	}
+	
+	
+	
 	
 	@Test
 	public void deleteTwitter()
